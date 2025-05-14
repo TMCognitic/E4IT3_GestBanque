28bfa542101caa4e3738366a11e5d69ec35b7d40
Thierry Morre <thierry.morre@cognitic.be>
2025-05-14 10:48:26 +0200

    Ajout Compte Epargne dans le programme

--- GestBanque/Program.cs
+++ GestBanque/Program.cs
@@ -16,17 +16,32 @@ Courant courant = new Courant()
     LigneDeCredit = 500
 };
 
+Epargne epargne = new Epargne()
+{
+    Numero = "Test002",
+    Titulaire = johnDoe
+};
+
 
 Console.WriteLine($"Compte numéro '{courant.Numero}' de {courant.Titulaire.Prenom} {courant.Titulaire.Nom}");
 courant.Depot(-500);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
 courant.Depot(500);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
 courant.Retrait(-100);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
 courant.Retrait(100);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
 courant.Retrait(900);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
 courant.Retrait(100);
-Console.WriteLine($"Nouveau solde : {courant.Solde}");
+Console.WriteLine($"Solde : {courant.Solde}");
+
+Console.WriteLine();
+Console.WriteLine($"Compte numéro '{epargne.Numero}' de {courant.Titulaire.Prenom} {courant.Titulaire.Nom}");
+epargne.Depot(500);
+Console.WriteLine($"Solde : {epargne.Solde}");
+epargne.Retrait(100);
+Console.WriteLine($"Solde : {epargne.Solde}");
+epargne.Retrait(500);
+Console.WriteLine($"Solde : {epargne.Solde}");
